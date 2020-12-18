c16b3ddaf06f48391fea3d39067ef51b99dbd7a2
Mons Pavel <monspavel@gmail.com>
2020-12-18 15:05:50 +0300

    add heroes constant

---
+++ src/utils/constants.js
@@ -0,0 +1,114 @@
+export const heroes = [
+    {
+        name: 'Amber',
+        stars: 4
+    },
+    {
+        name: 'Barbara',
+        stars: 4
+    },
+    {
+        name: 'Beidou',
+        stars: 4
+    },
+    {
+        name: 'Bennett',
+        stars: 4
+    },
+    {
+        name: 'Chongyun',
+        stars: 4
+    },
+    {
+        name: 'Diluc',
+        stars: 5
+    },
+    {
+        name: 'Diona',
+        stars: 4
+    },
+    {
+        name: 'Fischl',
+        stars: 4
+    },
+    {
+        name: 'Jean',
+        stars: 5
+    },
+    {
+        name: 'Kaeya',
+        stars: 4
+    },
+    {
+        name: 'Keqing',
+        stars: 5
+    },
+    {
+        name: 'Klee',
+        stars: 5
+    },
+    {
+        name: 'Lisa',
+        stars: 4
+    },
+    {
+        name: 'Mona',
+        stars: 5
+    },
+    {
+        name: 'Ningguang',
+        stars: 4
+    },
+    {
+        name: 'Noelle',
+        stars: 4
+    },
+    {
+        name: 'Qiqi',
+        stars: 5
+    },
+    {
+        name: 'Razor',
+        stars: 4
+    },
+    {
+        name: 'Sucrose',
+        stars: 4
+    },
+    {
+        name: 'Tartaglia',
+        stars: 5
+    },
+    {
+        name: 'Traveler (Anemo)',
+        stars: 5
+    },
+    {
+        name: 'Traveler (Geo)',
+        stars: 5
+    },
+    {
+        name: 'Venti',
+        stars: 5
+    },
+    {
+        name: 'Xiangling',
+        stars: 4
+    },
+    {
+        name: 'Xiao',
+        stars: 5
+    },
+    {
+        name: 'Xingqiu',
+        stars: 4
+    },
+    {
+        name: 'Xinyan',
+        stars: 4
+    },
+    {
+        name: 'Zhongli',
+        stars: 5
+    }
+]
